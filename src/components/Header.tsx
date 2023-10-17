38c1502d375546a1eec09187a0ffa4187b4d119e
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-10-16 22:35:38 -0300

    style: 💄 Global CSS

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.css'
+export const Header = () => {
+    return (
+        <strong className={styles.header}>
+            Ignite Feed
+        </strong>
+    )
+}
