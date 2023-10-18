c26fafe9a3322d018a537c0a3e1b808109fae2bf
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-10-17 22:35:27 -0300

    style: 💄 Post style

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -24,14 +24,17 @@ export const Post = () => {
                 </p>
 
                 <p>
-                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da
+                    Rocketseat. O nome do projeto é DoctorCare 🚀
                 </p>
 
                 <p>
-                    👉 <a href="#">jane.design/doctorcare</a>
+                    👉{' '} <a href="#">jane.design/doctorcare</a>
                 </p>
                 <p>
-                    <a href="#">novoprojeto #nlw #rocketseat </a>
+                    <a href="#"> #novoprojeto </a> {' '}
+                    <a href="#"> #nlw </a> {' '}
+                    <a href="#"> #rocketseat </a> {' '}
                 </p>
             </div>
         </article>
